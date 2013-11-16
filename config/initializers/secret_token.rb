9f791777fe293fc2192b4d5d04cfa8c62b55e117
Shane Ardell <shane.ardell@gmail.com>
2013-11-16 14:40:52 -0500

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Roadrage::Application.config.secret_key_base = '5e4bdcc09ccb8ba4cb84af391effda05db9b21288f6aacfd5b326faf0147d1a4b8e4cea89eb7c007a797b42487b8cd0526a9199b413441ab4d819efe69ae6276'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Roadrage::Application.config.session_store :cookie_store, key: '_roadrage_session'
